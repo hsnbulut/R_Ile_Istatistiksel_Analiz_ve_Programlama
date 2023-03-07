b978ce1b51daa60b1de3ef15b2ed9139b3f505d6
Assoc. Prof. Hasan Bulut <hasan.bulut@omu.edu.tr>
2023-03-07 11:27:08 +0300

    Delete Chapter 4 directory

--- Chapter 4/Example 4.4.R
+++
@@ -1,7 +0,0 @@
- A<-matrix(0,nrow=10, ncol=10)
- for (i in 1:10) {  # 1. Döngü başlangıcı
-     for (j in 1:10) { # 2. Döngü Başlangıcı
-         A[i,j]<-i*j
-       }  # 2. Döngü Bitişi
-   } # 1. Döngü Bitişi		
- A
